ca20a58b830f67fe72ae7c88b0dc82373c820bc2
Homero Meireles <homero.meireles@novahaus.com.br>
2024-03-21 18:16:18 -0300

    chore: add functions

--- app.js
+++ app.js
@@ -6,4 +6,68 @@ const computerSelection = () => {
   return choice;
 }
 
-let computerChoice = computerSelection();
+let message = '';
+
+const playRPSGame = () => {
+  let computerChoice = computerSelection();
+  let playerChoice = prompt('Enter paper, rock or scissors!');
+
+  playerChoice = playerChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+
+  if (playerChoice == computerChoice) {
+    message = 'Tie game!';
+  }
+
+  if (playerChoice == 'rock' && computerChoice == 'scissors') {
+    message = 'You win! Rock beats Scissors!'
+  }
+
+  if (playerChoice == 'rock' && computerChoice == 'paper') {
+    message = 'You lose! Paper beats Rock!'
+  }
+
+  if (playerChoice == 'paper' && computerChoice == 'rock') {
+    message = 'You win! Paper beats Rock!'
+  }
+
+  if (playerChoice == 'paper' && computerChoice == 'scissors') {
+    message = 'You lose! Scissors beats paper!'
+  }
+
+  if (playerChoice == 'scissors' && computerChoice == 'paper') {
+    message = 'You win! Scissors beats Paper!'
+  }
+
+  if (playerChoice == 'scissors' && computerChoice == 'rock') {
+    message = 'You lose! Rock beats Scissors!'
+  }
+
+  console.log(message);
+
+  return message;
+}
+
+const playGame = () => {
+  let i = 1;
+  let pointsPlayer = 0;
+  let pointsComputer = 0;
+
+  do {
+    playRPSGame();
+    i++;
+    if (message.includes('lose')) {
+      pointsComputer++;
+    }
+
+    if (message.includes('win')) {
+      pointsPlayer++;
+    }
+  } while(i > 6);
+
+
+  console.log(pointsPlayer, pointsComputer);
+}
+
+playGame();
